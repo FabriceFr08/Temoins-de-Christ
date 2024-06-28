bb187539262d5602e77b140a9f50d3144c117b34
micheline2022 <mmetueche@yob.fr>
2024-06-28 11:00:51 +0100

    Comment recevoir Christ en plusieurs versions

---
+++ resources/views/cmt_recevoir_christ.blade.php
@@ -0,0 +1,182 @@
+@extends('layouts.app')
+
+@section('title', 'Comment venir à Christ')
+
+@section('content')
+
+<div class="container-fluid training py-5">
+    <div class="container-fluid training py-5">
+        <div  class="row g-5">
+            <div class="col-md-6  order-first">
+                <div class="card bg-light">
+                    <h3 class="text-secondary fw-bold text-center">COMMENT DONNER SA VIE A CHRIST</h3>
+                    <div class="card-header mb-2  text-dark">
+                        <img src="img/donner_vie_christ/questions_reponses.jpg" class="card-img" alt="Vos questions.">
+                    </div>
+                    
+                    <div class="card-body">
+                        <h5 class="card-title text-success mb-2">VOS QUESTIONS FAMILIERES</h5>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Aimeriez-Vous Entendre Dieu ?</li>
+                            <li class="list-group-item">Aimeriez-Vous Découvrir Pourquoi Vous Êtes Sur Terre ?</li>
+                            <li class="list-group-item">Aimeriez-Vous Expérimenter La Vie Éternelle Ici-Bas ?</li>
+                            <li class="list-group-item">Etes-Vous Fatigué De Vos Faiblesses, Défauts Et Limitations ?</li>
+                            <li class="list-group-item">Etes-Vous Fatigué De Subir La Vie Et Ses Déceptions ?</li>
+                            <li class="list-group-item">Voulez-Vous Transformer Vos Échecs En  Réussite Et Succès ?</li>
+                            <li class="list-group-item">Avez-Vous Peur De L'inconnu Et De La Mort ?</li>
+                            <li class="list-group-item">Voulez-Vous Enfin Découvrir La Solution Qui Changera Votre Vie ?</li>
+                            <li class="list-group-item">Savez-Vous Qu'il Y A Une Solution Pour Chacun De Vos Problèmes ?</li>
+                            <li class="list-group-item">Voulez-Vous Savoir Comment Recevoir Puis Manifester L'amour De Dieu ?</li>
+                            <li class="list-group-item">Savez-Vous Que Dieu Veut Vous Guérir Puis Vous Offrir La Santé Divine ?</li>
+                            <li class="list-group-item">Recherchez-Vous Un Ami Fidèle Pour Le Reste De Votre Vie </li>    
+                        </ul>
+                    </div>
+                </div>                
+                <div class="card-header" >
+                    <h5 class="card-title text-success mb-3"> LA REPONSE A TOUTES CES QUESTIONS CONSISTE A NAITRE DE NOUVEAU</h5>
+
+                    <p class="card-text fw-bold text-secondary">En Effet Dans Jean 3 : 5-6 Lsg, Le Seigneur A Justement Dit Ceci À Nicodème :  « 5
+                        En Vérité, En Vérité, Je Te Le Dis, Si Un Homme Ne Naît De Nouveau, Il Ne Peut 
+                        Voir Le Royaume De Dieu. 6 Ce Qui Est Né De La Chair Est Chair, Et Ce Qui Est Né De L’esprit Est Esprit»
+                    </p>                                             
+                </div>
+                <div class="card-body bg-light">
+                    <p class="card-text fw-bold mb-2 text-secondary">CAR :</p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">On peut croire en Dieu. <b>Et ne pas naître de nouveau</b></li>
+                        <li class="list-group-item">On peut croire en l’existence du Seigneur Jésus. <b>Et ne pas naître de nouveau.</b></li>
+                        <li class="list-group-item">On peut croire en l’existence du Seigneur Jésus. <b>Et ne pas naître de nouveau.</b></li>
+                        <li class="list-group-item">On peut connaitre toute la Bible par cœur.<b> Et ne pas naître de nouveau.</b></li>
+                        <li class="list-group-item"> On peut faire beaucoup de bonnes œuvres, beaucoup d’aumônes aux pauvres, beaucoup d’offrandes à l’église. <b>Et ne pas naître de nouveau.</b></li>
+                        <li class="list-group-item">On peut beaucoup prier (les païens aussi prient, a dit  le Seigneur Jésus; Matthieu 6 :7).<b> Et ne pas naître de nouveau</b></li>
+                        <li class="list-group-item">On peut être une personne extrêmement gentille et douce, très disciplinée, et qui ne provoque jamais personne (Marc 10 :17-22). <b>Et ne pas naître de nouveau.</b></li>
+                        <li class="list-group-item">On peut aller à l’église chaque dimanche depuis son enfance, que ce soit à l’église catholique, évangélique, protestante, pentecôtiste, etc. <b>Et ne pas naître de nouveau.</b></li>
+                        <li class="list-group-item">On peut aller à l’église chaque dimanche depuis son enfance, que ce soit à l’église catholiq</li>
+                    </ul>
+                </div>
+                <div class="card bg-light">
+                    <div class="card-header ">
+                        <h3 class="text-secondary fw-bold text-center">VOICI LA CONFESSION A FAIRE DE TOUT VOTRE COEUR</h3>
+                        <img src="img/donner_vie_christ/repentance.jpg" class="card-img" alt="Nouvelle naissance">                 
+                    </div>       
+                    <div class="card-body mb-2">                        
+                        <p class="text-dark"><b>Dans Jean 1 : 12-13 LSG</b> la Bible déclare : « Mais à tous ceux 
+                            qui l’ont reçue, à ceux qui croient en son nom, elle a donné 
+                            le pouvoir de devenir enfants de Dieu, 13 lesquels sont nés, 
+                            non du sang, ni de la volonté de la chair, ni de la volonté de 
+                            l’homme, mais de Dieu ».
+                        </p>   
+                        <p class="text-dark fw-bold">« Car Dieu a tant aimé le monde qu’il a donné 
+                            son Fils unique, afin que quiconque croit en lui ne périsse point,
+                            mais qu’il ait la vie éternelle. Dieu, en effet, n’a pas envoyé son Fils 
+                            dans le monde pour qu’il juge le monde, mais  pour que le monde soit sauvé par lui. 
+                            Celui qui croit en lui n’est point jugé ; mais celui qui ne croit pas est déjà jugé, 
+                            parce qu’il n’a pas cru au nom du Fils unique de Dieu »
+                    </div>
+                </div>                               
+            </div>
+            <div class="col-md-6">
+                <div class="card bg-light">
+                    <h3 class="text-secondary fw-bold text-center">COMMENT NAITRE DE NOUVEAU ?</h3>
+                    <div class="card-header mb-2 text-dark">
+                        <img src="img/donner_vie_christ/nouvelle_naissance.jpg" class="card-img" alt="Nouvelle naissance">
+                    </div>
+                                        
+                    <div class="card-body">
+                        <h5 class="card-title text-success my-2">JEAN 3 :16-18 LSG</h5>
+                        <p class="text-dark fw-bold">« Car Dieu a tant aimé le monde qu’il a donné 
+                            son Fils unique, afin que quiconque croit en lui ne périsse point,
+                            mais qu’il ait la vie éternelle. Dieu, en effet, n’a pas envoyé son Fils 
+                            dans le monde pour qu’il juge le monde, mais  pour que le monde soit sauvé par lui. 
+                            Celui qui croit en lui n’est point jugé ; mais celui qui ne croit pas est déjà jugé, 
+                            parce qu’il n’a pas cru au nom du Fils unique de Dieu »
+                        </p>                                      
+                                        
+                        <h5 class="card-title text-success">JEAN 3 : 36 LSG</h5>
+                        <p class="text-dark fw-bold"> Celui qui croit au Fils a la vie éternelle ; 
+                            celui qui ne croit pas au Fils ne verra point la 
+                            vie, mais la colère de Dieu demeure sur lui ».
+                        </p>                    
+                        
+                        <h5 class="card-title text-success mb-2">ACTES 4 :12</h5>
+                        <p class="text-dark fw-bold"> « Il n’y a de salut en aucun autre ; 
+                            car il n’y a sous le ciel aucun autre nom qui ait été 
+                            donné parmi les hommes, par lequel nous devions être sauvés ».
+                        </p>                   
+                                   
+                        <h5 class="card-title text-success mb-2">1 CORINTHIENS 15 1-5 LSG</h5>
+                        <p class="text-dark fw-bold">« Je vous rappelle, frères, l’Evangile que je 
+                            vous ai annoncé, que vous avez reçu, dans lequel vous avez persévéré, 
+                            2 et par lequel vous êtes sauvés, si vous le retenez tel que 
+                            je vous l’ai annoncé ; autrement, vous auriez cru en vain. 
+                            3 Je vous ai enseigné avant tout, comme je l’avais aussi reçu, que Christ est 
+                            mort pour nos péchés, selon les Ecritures ; 4 qu’il a été enseveli, 
+                            et qu’il est ressuscité le troisième jour, selon les Ecritures ; 5 et qu’il 
+                            est apparu à Céphas, puis aux douze ».
+                        </p>                    
+                
+                    
+                        <h5 class="card-title text-success mb-2">ROMAINS 10 : 9-11,13</h5>
+                        <p class="fw-bold text-dark">« Si tu confesses de ta bouche le Seigneur 
+                            Jésus, et si tu crois dans ton cœur que Dieu 
+                            l’a ressuscité des morts, tu seras sauvé. 10 
+                            Car c’est en croyant du cœur qu’on parvient à 
+                            la justice, et c’est en confessant de la bouche 
+                            qu’on parvient au salut, 11 selon ce que dit 
+                            l’Ecriture : Quiconque croit en lui ne sera 
+                            point confus… Car quiconque invoquera le 
+                            nom du Seigneur sera sauvé ».
+                        </p>                                      
+                    </div> 
+                </div>                
+                <div class="card  bg-light">                
+                    <div class="card-header">
+                        <h3 class="text-secondary fw-bold text-center">BIENVENUE DANS LA FAMILLE DES RACHETÉS</h3>
+                        <img src="img/donner_vie_christ/bienvenue1.jpg" class="card-img" alt="Nouvelle naissance">                 
+                    </div>                                                    
+                    <div class="card-body">
+                        <p class=" text-dark">Nous nous réjouissons avec 
+                            vous <b>(Romains 12 :15)</b>, le Seigneur et les anges aussi 
+                            se réjouissent pour votre retour à la maison (<b>Luc 15 : 
+                            7, 10</b>). Ne gardez pas cette bonne nouvelle pour vous seul, 
+                            parlez-en autour de vous, proclamez les bontés de Dieu, 
+                            et trouvez dès à présent une église qui glorifie le Seigneur Jésus
+                        </p>
+                        <p class=" text-dark m-0">Envoyez aussi ce message :
+                            <p class="text-success fw-bold m-0">JE SUIS SAUVE</p> 
+                            <p class="text-dark">à ce numéro :</p>
+                            <p class="text-success fw-bold m-0">WhatsApp SVP : +33767744933.                       
+                            </p>
+                            <p class=" text-dark">Et nous vous contacterons, en retour, pour un accompagnement dans 
+                            la famille de Dieu</p>
+                        </p>
+                        <p class="card-text fw-bold text-success">
+                            JESUS-CHRIST EST SEIGNEUR
+                        </p>                           
+                    </div>
+                </div>
+                <div class="card bg-light">                    
+                    <h3 class="text-secondary fw-bold text-center">COMMENT DONNER SA VIE A CHRIST : AUTRES VERSIONS</h3>
+                    <h5 class="card-title text-success">VERSION PDF</h5>
+
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe src="{{ asset('pdf/DONNER-SA-VIE-A-CHRIST.pdf') }}" title="version pdf" allowfullscreen width="100%" height="360"></iframe>
+                    </div>
+                    <h5 class="card-title text-success mt-2">VERSION AUDIO</h5>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <audio src="{{ asset('video/This_is_two_way.mp3') }}" controls width="100%"></audio>
+                    </div>
+                    <h5 class="card-title text-success mt-2">VERSION VIDEO</h5>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <video src="{{ asset('video/video.mp4') }}" controls type="video/pm4" width="100%" height="360" ></video>
+                    </div>
+
+                    
+                </div>
+            </div>
+
+        </div>    
+        
+    <div>
+</div>
+@endsection
