2be0e24cd7b15a42e8374b6da61275cc8adc3d3d
FabriceFr08 <fabriceessovi5@gmail.com>
2024-08-30 17:38:18 +0100

    Optimisation du fichier de routes

--- routes/web.php
+++ routes/web.php
@@ -2,121 +2,86 @@
 
 use App\Http\Controllers\CommentaireController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\reseau_tdc\InscriptionController;
+use App\Http\Controllers\reseau_tdc\ReseauController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategorieArticleController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\FrontController;
 
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () { /* Routes pour la partie administration*/
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+    Route::resource('categories', CategorieArticleController::class);  // Routes pour les catégories d'articles
+    Route::resource('villes', \App\Http\Controllers\VilleController::class);  // Routes pour les villes
+    Route::resource('articles', ArticleController::class); // Route pour les articles
+});  // Routes pour la partie administration
+Route::prefix('formations')->group(function () {
+    Route::get('/', function () {
+        return view('formations.index');
+    })->name('formations.index');
 
-use App\Http\Controllers\CategorieArticleController;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\FrontController;
-use App\Http\Controllers\FormationController;
+    Route::prefix('nos-formations')->group(function () {
+        Route::get('/', function () {
+            return view('formations.nos_formations');
+        })->name('nos-formations');
 
+        Route::get('/premiers-pas', function () {
+            return view('formations.pages.premier_pas');
+        })->name('nos-formations.premier_pas');
 
-Route::get('/', function () {
-    return view('index');
-});
+        Route::get('/devenir-temoin-de-christ', function () {
+            return view('formations.pages.devenir_tdc');
+        })->name('nos-formations.devenir_tdc');
 
-Route::get('/formations/nos-formations', function () {
-    return view('formations.nos_formations');
-})->name('nos-formations');
+        Route::get('/comment-servir-dieu', function () {
+            return view('formations.pages.comment_servir');
+        })->name('nos-formations.comment-servir-dieu');
+    });
+});  // Routes pour la section formation
+Route::prefix('reseau')->group(function () {
+    Route::get('/send-verification-code', [ReseauController::class, 'verificationCode'])->name('verificationCode');
+    Route::post('/send-verification-code', [ReseauController::class, 'sendVerificationCode'])->name('sendVerificationCode');
 
-Route::get('/formations/nos-formations/premiers-pas', function () {
-    return view('formations.pages.premier_pas');
-})->name('nos-formations.premier_pas');
+    Route::prefix('verify-code/{prestataire}')->group(function () {
+        Route::get('/', [ReseauController::class, 'showVerificationForm'])->name('showVerificationForm');
+        Route::post('/', [ReseauController::class, 'verifyCode'])->name('verifyCode');
+    });
 
-Route::get('/formations/nos-formations/sevenir-temoin-de-christ', function () {
-    return view('formations.pages.devenir_tdc');
-})->name('nos-formations.devenir_tdc');
-
-Route::get('/formations/nos-formations/comment-servir-dieu', function () {
-    return view('formations.pages.omment_servir');
-})->name('nos-formations.comment-servir-dieu');
-
-
-Route::get('/victoire_pour_christ', function () {
-    return view('venir_a_christ');
-})->name('victoire_pour_christ');
-
-Route::get('/venir-a-christ', function () {
-    return view('cmt_recevoir_christ');
-})->name('venir-a-christ');
-
-Route::get('/victoire_pour_christ', function () {
-    return view('venir_a_christ');
-})->name('victoire_pour_christ');
-
-
-Route::get('/but-vision-mission', function () {
-    return view('a propos.but_vision_mission');
-})->name('but-vision-mission');
-
-Route::get('/victoire-avec-christ', function () {
-    return view('victoire_avec_christ');
-})->name('venir-a-christ1');
-
-Route::get('/reseau', function () {
-    return view('reseau_tdc.index');
-})->name('reseau');
-
-Route::get('/profil', function () {
-    return view('reseau_tdc.show');
-})->name('profil');
-
-Route::controller(\App\Http\Controllers\reseau_tdc\InscriptionController::class)->group(function () {
-    Route::get('/reseau/services', 'index')->name('reseau.services');
-    Route::get('/reseau/services/{hashedId}', 'show')->name('reseau.services.show');
-    Route::get('/reseau/inscription', 'create')->name('reseau.inscription');
-    Route::post('/reseau/inscription', 'store')->name('reseau.store');
-    Route::get('/reseau/filter', 'filter')->name('services.filter');
-});
-
-Route::get('/reseau/send-verification-code', [\App\Http\Controllers\reseau_tdc\ReseauController::class, 'verificationCode'])->name('verificationCode');
-Route::post('/reseau/send-verification-code', [\App\Http\Controllers\reseau_tdc\ReseauController::class, 'sendVerificationCode'])->name('sendVerificationCode');
-Route::get('/reseau/verify-code/{prestataire}', [\App\Http\Controllers\reseau_tdc\ReseauController::class, 'showVerificationForm'])->name('showVerificationForm');
-Route::post('/reseau/verify-code/{prestataire}', [\App\Http\Controllers\reseau_tdc\ReseauController::class, 'verifyCode'])->name('verifyCode');
-Route::get('/reseau/update-prestataire/{prestataire}/{code}', [\App\Http\Controllers\reseau_tdc\ReseauController::class, 'edit'])->name('updatePrestataire');
-Route::post('/reseau/update-prestataires/{prestataire}', [\App\Http\Controllers\reseau_tdc\ReseauController::class, 'update'])->name('update');
+    Route::prefix('update-prestataire/{prestataire}')->group(function () {
+        Route::get('/{code}', [ReseauController::class, 'edit'])->name('updatePrestataire');
+        Route::post('/', [ReseauController::class, 'update'])->name('update');
+    });
+});  // Routes pour la modification d'informations d'un utilisateur
+Route::prefix('reseau')->controller(InscriptionController::class)->group(function () {
+    Route::get('/services', 'index')->name('reseau.services');
+    Route::get('/services/{hashedId}', 'show')->name('reseau.services.show');
+    Route::get('/inscription', 'create')->name('reseau.inscription');
+    Route::post('/inscription', 'store')->name('reseau.store');
+    Route::get('/filter', 'filter')->name('services.filter');
+});  // Route pour inscription des prestataires et filtre
 
 
-
-Route::get('/don', function () {
-    return view('don');
-})->name('don');
+Route::view('/', 'index')->name('index');
+Route::view('/don', 'don')->name('don');
+Route::view('/victoire_pour_christ', 'venir_a_christ')->name('victoire_pour_christ');
+Route::view('/venir-a-christ', 'cmt_recevoir_christ')->name('venir-a-christ');
+Route::view('/but-vision-mission', 'a propos.but_vision_mission')->name('but-vision-mission');
+Route::view('/victoire-avec-christ', 'victoire_avec_christ')->name('venir-a-christ1');
+Route::view('/reseau', 'reseau_tdc.index')->name('reseau');
+Route::view('/profil', 'reseau_tdc.show')->name('profil');
 
 
 Route::get('/se-deployer', [FrontController::class, 'articles'])->name('se-deployer');
-
-Route::get('/accueil', [FrontController::class, 'index'])->name('index');
 Route::get('/article/{article}', [FrontController::class, 'showArticle'])->name('article.show');
+Route::post('/articles/{article}/commentaires', [CommentaireController::class, 'store'])->name('commentaires.store'); // Commentaire d'un article
+Route::post('/check-email', [\App\Http\Controllers\reseau_tdc\InscriptionController::class, 'checkEmail'])->name('check.email');  // Vérificaion d'email
 
-Route::post('/check-email', [\App\Http\Controllers\reseau_tdc\InscriptionController::class, 'checkEmail'])->name('check.email');
-
-
-Route::resource('formations', FormationController::class)->except('destroy', 'show', 'update', 'edit');  //
-
-
-Route::middleware('auth')->resource('categories', CategorieArticleController::class);  // Routes pour les catégories d'articles
-Route::middleware('auth')->resource('villes', \App\Http\Controllers\VilleController::class);  // Routes pour les villes
-
-
-Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
-Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
-Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
-Route::put('/articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
-Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
-
-
-Route::post('/articles/{article}/commentaires', [CommentaireController::class, 'store'])->name('commentaires.store');
+require __DIR__.'/auth.php';
