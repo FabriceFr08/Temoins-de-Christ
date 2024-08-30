58b27fdf25789d9ef71548718f9930bdd8fe7700
FabriceFr08 <fabriceessovi5@gmail.com>
2024-08-30 17:33:28 +0100

    Optimisation du fichier de routes

--- resources/views/back/articles/edit.blade.php
+++ resources/views/back/articles/edit.blade.php
@@ -13,7 +13,7 @@
                     <div class="row breadcrumbs-top d-inline-block">
                         <div class="breadcrumb-wrapper col-12">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{ route('blog') }}">Accueil</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('se-deployer') }}">Accueil</a></li>
                                 <li class="breadcrumb-item"><a href="#">Articles</a></li>
                                 <li class="breadcrumb-item active">Modifier un article</li>
                             </ol>
